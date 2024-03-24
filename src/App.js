30acd6564114276cea277e1cb2b0304cf1b59ac4
forward28 <forward28@inbox.lv>
2024-03-24 14:51:44 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://aqueous-plains-32159.herokuapp.com:3000/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/signin", {
+    fetch("https://aqueous-plains-32159-7fae57eabbc3.herokuapp.com/signin", {
       method: "post",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({
